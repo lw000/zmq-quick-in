70dec3f91770e5136f1a2f93f9b132bbb73b0c08
lw000 <373102227@qq.com>
2023-11-25 20:44:08 +0800

    添加异常处理

--- zmq-proxy/zmq-proxy.cpp
+++ zmq-proxy/zmq-proxy.cpp
@@ -14,8 +14,27 @@ void ProxyThread2(zmq::context_t* ctx)
 {
     zmq::socket_t frontend(*ctx, zmq::socket_type::router);
     zmq::socket_t backend(*ctx, zmq::socket_type::dealer);
-    frontend.bind("tcp://*:5558");
-    backend.bind("tcp://*:5559");
+
+    bool init = false;
+
+    do
+    {
+        try
+        {
+            frontend.bind("tcp://*:5558");
+            backend.bind("tcp://*:5559");
+            init = true;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    } while (0);
+   
+    if (!init)
+    {
+        return;
+    }
 
     zmq_pollitem_t poll_items[] =
     {
@@ -28,54 +47,65 @@ void ProxyThread2(zmq::context_t* ctx)
         zmq::message_t msg;
         size_t more = 0;
 
-        int rc = zmq::poll(poll_items, 2);
-        if (rc == -1)
+        try
         {
-            break;
-        }
+            int rc = zmq::poll(poll_items, 2, std::chrono::milliseconds(100));
+            // zmq::poll出错
+            if (rc == -1)
+            {
+                break;
+            }
+            // zmq::poll超时
+            if (rc == 0)
+            {
+                continue;
+            }
 
-        if (rc == 0)
+            // ROUTER 套接字有数据来
+            if (poll_items[0].revents & ZMQ_POLLIN)
+            {
+                while (1)
+                {
+                    frontend.recv(msg);
+
+                    std::cout << "frontend " << msg << std::endl;
+
+                    more = frontend.get(zmq::sockopt::rcvmore);
+
+                    backend.send(msg, more ? zmq::send_flags::sndmore : zmq::send_flags::none);
+
+                    if (!more)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // DEALER套接字有数据来
+            if (poll_items[1].revents & ZMQ_POLLIN)
+            {
+                while (1)
+                {
+
+                    backend.recv(msg);
+
+                    std::cout << "backend " << msg << std::endl;
+
+                    more = backend.get(zmq::sockopt::rcvmore);
+                    frontend.send(msg, more ? zmq::send_flags::sndmore : zmq::send_flags::none);
+
+                    if (!more)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        catch (const std::exception& e)
         {
-            continue;
+            std::cerr << e.what() << std::endl;
         }
         
-        if (poll_items[0].revents & ZMQ_POLLIN)
-        {
-            while (1)
-            {
-                frontend.recv(msg);
-
-                std::cout << "frontend " << msg << std::endl;
-
-                more = frontend.get(zmq::sockopt::rcvmore);
-
-                backend.send(msg, more ? zmq::send_flags::sndmore : zmq::send_flags::none);
-
-                if (!more)
-                {
-                    break;
-                }
-            }
-        }
-
-        if (poll_items[1].revents & ZMQ_POLLIN)
-        {
-            while (1)
-            {
-
-                backend.recv(msg);
-
-                std::cout << "backend " << msg << std::endl;
-
-                more = backend.get(zmq::sockopt::rcvmore);
-                frontend.send(msg, more ? zmq::send_flags::sndmore : zmq::send_flags::none);
-
-                if (!more)
-                {
-                    break;
-                }
-            }
-        }
     }
 }
 
@@ -85,7 +115,7 @@ int main(int argc, char** argv)
     zmq::version(&major, &minor, &patch);
     printf("ZMQ Version: %d.%d.%d\n", major, minor, patch);
 
-    zmq::context_t ctx(8);
+    zmq::context_t ctx(4);
 
     auto thread2 = std::async(std::launch::async, ProxyThread2, &ctx);
 
